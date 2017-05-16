330bd9b6df53f40de20a2b6a16d72e2dfe7432c5
ArcAick <j.rem.s91@live.fr>
2017-05-16 17:15:44 +0200

    try correction 4

--- app.js
+++ app.js
@@ -7,32 +7,33 @@ AWS.config.update({
     region: "us-west-2"
 });
 
-app.get('/', function(req, res) {
-	res.send('Wow');
+app.get('/', function (req, res) {
+    res.send('Wow - CI');
 });
 
-app.get('/user', function(req, res){
-	
+app.get('/users', function (req, res) {
+    
 });
 
-app.get('/user/:userId', function(req, res) {
-	var docClient = new AWS.DynamoDB.DocumentClient();
-	var params = {
-		TableName : "users",
-		Key : {
-			"id_user": req.params["userId"]
-		}
-	};
+
+app.get('/user/:userId', function (req, res) {
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "users",
+        Key:{
+            "id_user": req.params["userId"]
+        }
+    };
 	
-	docClient.get(params, function(err, data) {
-		if(err) {
-			res.send("error while reading: " + JSON;stringify(err, null, 2));
-		} else {
-			res.send("get item: " + JSON.stringify(data, null, 2));
-		}
-	});
+    docClient.get(params, function(err, data) {
+        if (err) {
+            res.send("Unable to read item. Error JSON:" + JSON.stringify(err, null, 2));
+        } else {
+            res.send("GetItem succeeded:" + JSON.stringify(data, null, 2));
+        }
+    });
 });
 
 app.listen(process.env.PORT, function () {
-    console.log("server successfully started!");
+    console.log("Eventually server successfully started!");
 });
